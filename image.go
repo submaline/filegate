443f5274def143924c2b49cc9c0b786f7f52c10b
x0y14 <ya01ma08@gmail.com>
2022-07-12 22:47:33 +0900

    icon用と通常画像用を用意

--- image.go
+++ image.go
@@ -14,9 +14,18 @@ func ResizeAndConvertToWebp(buf []byte, width, height int) ([]byte, error) {
 }
 
 func CompressNAndConvertWebp(buf []byte, percent int) ([]byte, error) {
-	webpped, err := bimg.NewImage(buf).Convert(bimg.WEBP)
+	webpImg, err := bimg.NewImage(buf).Convert(bimg.WEBP)
 	if err != nil {
 		return nil, err
 	}
-	return bimg.NewImage(webpped).Process(bimg.Options{Quality: percent})
+	return bimg.NewImage(webpImg).Process(bimg.Options{Quality: percent})
+}
+
+func ThumbnailWebp(buf []byte) ([]byte, error) {
+	// アスペクト比保たれて、黒埋めされる。
+	resized, err := bimg.NewImage(buf).Resize(640, 480)
+	if err != nil {
+		return nil, err
+	}
+	return bimg.NewImage(resized).Process(bimg.Options{Quality: 80})
 }
--- main.go
+++ main.go
@@ -20,6 +20,13 @@ import (
 
 var s3Client *s3.Client
 
+type JsonResponse struct {
+	Message       string `json:"message"`
+	FileName      string `json:"file_name"`
+	FilePath      string `json:"file_path"`
+	ThumbnailPath string `json:"thumbnail_path"`
+}
+
 func init() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("failed to load env file")
@@ -45,23 +52,24 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.POST("/upload", func(c *gin.Context) {
+	r.POST("/upload/icon", func(c *gin.Context) {
 		file, _, err := c.Request.FormFile("files")
 		if err != nil {
 			log.Printf("failed to get file: %v\n", err)
-			c.JSON(http.StatusBadRequest, "{\"message\": \"unknown\"}")
+
+			c.JSON(http.StatusBadRequest, JsonResponse{Message: "unknown"})
 			return
 		}
 		buf, err := io.ReadAll(file)
 		if err != nil {
 			log.Printf("failed to read file: %v\n", err)
-			c.JSON(http.StatusBadRequest, "{\"message\": \"failed to read file\"}")
+			c.JSON(http.StatusBadRequest, JsonResponse{Message: "failed to read file"})
 			return
 		}
 
 		mime, ok := IsAllowedFileType(buf)
 		if !ok {
-			c.JSON(http.StatusForbidden, fmt.Sprintf("{\"message\": \"not allowed file type: %v\"}", mime))
+			c.JSON(http.StatusForbidden, JsonResponse{Message: fmt.Sprintf("not allowed file type: %v", mime)})
 			return
 		}
 		filename := uuid.NewString()
@@ -70,7 +78,7 @@ func main() {
 		i250x250, err := ResizeAndConvertToWebp(buf, 250, 250)
 		if err != nil {
 			log.Printf("failed to process & convert image: %v\n", err)
-			c.JSON(http.StatusInternalServerError, "{\"message\": \"failed to process & convert file\"}")
+			c.JSON(http.StatusInternalServerError, JsonResponse{Message: "failed to process & convert file"})
 			return
 		}
 
@@ -80,90 +88,91 @@ func main() {
 			Bucket: aws.String(os.Getenv("R2_BUCKET_NAME")),
 			Key:    aws.String(filepath),
 			Body:   bytes.NewBuffer(i250x250),
-			//ContentType: aws.String("image/webp"),
 		})
 
 		if err != nil {
 			log.Printf("failed to upload file to s3: %v", err)
-			c.JSON(http.StatusInternalServerError, "{\"message\": \"failed to upload file\"}")
+			c.JSON(http.StatusInternalServerError, JsonResponse{Message: "failed to upload file"})
 			return
 		}
 
-		c.JSON(http.StatusOK, fmt.Sprintf("{\"message\": \"success\", \"filename\": \"%s\"}", filename))
+		c.JSON(http.StatusOK, JsonResponse{
+			Message:  "success",
+			FileName: filename,
+			FilePath: filepath,
+		})
 	})
 
-	//r.POST("/upload/image", func(c *gin.Context) {
-	//	var sess *session.Session
-	//	go func() {
-	//		sess = session.Must(session.NewSessionWithOptions(session.Options{
-	//			Profile:           os.Getenv("AWS_PROFILE"),
-	//			SharedConfigState: session.SharedConfigEnable,
-	//			Config: aws.Config{
-	//				Region: aws.String("ap-northeast-1"),
-	//			},
-	//		}))
-	//	}()
-	//
-	//	file, _, err := c.Request.FormFile("files")
-	//	if err != nil {
-	//		log.Printf("failed to get file: %v\n", err)
-	//		c.JSON(http.StatusBadRequest, "{\"message\": \"unknown\"}")
-	//		return
-	//	}
-	//	buf, err := io.ReadAll(file)
-	//	if err != nil {
-	//		log.Printf("failed to read file: %v\n", err)
-	//		c.JSON(http.StatusBadRequest, "{\"message\": \"failed to read file\"}")
-	//		return
-	//	}
-	//
-	//	mime, ok := IsAllowedFileType(buf)
-	//	if !ok {
-	//		c.JSON(http.StatusForbidden, fmt.Sprintf("{\"message\": \"not allowed file type: %v\"}", mime))
-	//		return
-	//	}
-	//	filename := uuid.NewString()
-	//	log.Printf("filename: %v\n", filename)
-	//	uploader := s3manager.NewUploader(sess)
-	//
-	//	iQuo80, err := CompressNAndConvertWebp(buf, 80)
-	//	if err != nil {
-	//		log.Printf("failed to compress: %v\n", err)
-	//		c.JSON(http.StatusInternalServerError, "{\"message\": \"failed to compress file\"}")
-	//		return
-	//	}
-	//	quo80Filepath := fmt.Sprintf("q80/%s", filename)
-	//	_, err = uploader.Upload(&s3manager.UploadInput{
-	//		Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
-	//		Body:   aws.ReadSeekCloser(bytes.NewBuffer(iQuo80)),
-	//		Key:    aws.String(quo80Filepath),
-	//	})
-	//	if err != nil {
-	//		log.Printf("failed to upload file to s3: %v", err)
-	//		c.JSON(http.StatusInternalServerError, "{\"message\": \"failed to upload file\"}")
-	//		return
-	//	}
-	//
-	//	thumbnail, err := ResizeAndConvertToWebp(buf, 640, 480)
-	//	if err != nil {
-	//		log.Printf("failed to resize & convert: %v\n", err)
-	//		c.JSON(http.StatusInternalServerError, "{\"message\": \"failed to compress file\"}")
-	//		return
-	//	}
-	//	thumbnailFilePath := fmt.Sprintf("thumb/%s", filename)
-	//	_, err = uploader.Upload(&s3manager.UploadInput{
-	//		Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
-	//		Body:   aws.ReadSeekCloser(bytes.NewBuffer(thumbnail)),
-	//		Key:    aws.String(thumbnailFilePath),
-	//	})
-	//	if err != nil {
-	//		log.Printf("failed to upload file to s3: %v", err)
-	//		c.JSON(http.StatusInternalServerError, "{\"message\": \"failed to upload file\"}")
-	//		return
-	//	}
-	//
-	//	c.JSON(http.StatusOK, fmt.Sprintf("{\"message\": \"success\", \"filename\": \"%s\"}", filename))
-	//})
+	r.POST("/upload/image", func(c *gin.Context) {
+		file, _, err := c.Request.FormFile("files")
+		if err != nil {
+			log.Printf("failed to get file: %v\n", err)
+			c.JSON(http.StatusBadRequest, JsonResponse{Message: "unknown"})
+			return
+		}
+		buf, err := io.ReadAll(file)
+		if err != nil {
+			log.Printf("failed to read file: %v\n", err)
+			c.JSON(http.StatusBadRequest, JsonResponse{Message: "failed to read file"})
+			return
+		}
+
+		mime, ok := IsAllowedFileType(buf)
+		if !ok {
+			c.JSON(http.StatusForbidden, JsonResponse{Message: fmt.Sprintf("not allowed file type: %v", mime)})
+			return
+		}
+		filename := uuid.NewString()
+		log.Printf("filename: %v\n", filename)
+
+		var filepath string
+		go func() { // 80%クオリティ
+			compressedImg, err := CompressNAndConvertWebp(buf, 80)
+			if err != nil {
+				log.Printf("failed to process & convert image: %v\n", err)
+				c.JSON(http.StatusInternalServerError, JsonResponse{Message: "failed to process & convert file"})
+				return
+			}
+			filepath = fmt.Sprintf("q80/%s", filename)
+			//
+			_, err = s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+				Bucket: aws.String(os.Getenv("R2_BUCKET_NAME")),
+				Key:    aws.String(filepath),
+				Body:   bytes.NewBuffer(compressedImg),
+			})
+			//
+			if err != nil {
+				log.Printf("failed to upload file to s3: %v", err)
+				c.JSON(http.StatusInternalServerError, JsonResponse{Message: "failed to upload file"})
+				return
+			}
+		}()
+
+		// サムネイル
+		thumb, err := ThumbnailWebp(buf)
+		if err != nil {
+			log.Printf("failed to process & convert image: %v\n", err)
+			c.JSON(http.StatusInternalServerError, JsonResponse{Message: "failed to process & convert file"})
+			return
+		}
+		_, err = s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+			Bucket: aws.String(os.Getenv("R2_BUCKET_NAME")),
+			Key:    aws.String("thumbnail/" + filename),
+			Body:   bytes.NewBuffer(thumb),
+		})
+		if err != nil {
+			log.Printf("failed to upload file to s3: %v", err)
+			c.JSON(http.StatusInternalServerError, JsonResponse{Message: "failed to upload file"})
+			return
+		}
+
+		c.JSON(http.StatusOK, JsonResponse{
+			Message:       "success",
+			FileName:      filename,
+			FilePath:      filepath,
+			ThumbnailPath: "thumbnail/" + filename,
+		})
+	})
 
 	if err := r.Run("0.0.0.0:8080"); err != nil {
 		log.Fatalf("failed to run gin: %v", err)
